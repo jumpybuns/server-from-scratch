1cc8d7739091fb90b07908ae7e566ceb50e782da
Ethan Pierce <ethanpiercepresents@gmail.com>
2023-01-14 12:48:39 -0800

    adds json data and endpoints

--- routes.js
+++ routes.js
@@ -1,10 +1,24 @@
 const http = require("http");
 const port = 6969;
 const host = 'localhost';
+const books = require('./data/books.json');
+const authors = require('./data/authors.json');
  
 const requestListener = (req, res) => {
-  res.writeHead(200);
-  res.end("Its amazing what you can make from scratch these days.");
+  res.setHeader("Content-Type", "application/json");
+  switch(req.url){
+    case "/books":
+      res.writeHead(200);
+      res.end(JSON.stringify(books));
+      break
+    case "/authors":
+      res.writeHead(200);
+      res.end(JSON.stringify(authors));
+      break
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: 'not found' }));
+  }
 };
 
 const server = http.createServer(requestListener);
